1961af75628b7e551b51f2278260cc3a09933f37
Marcin Kozina <mkozina@sigma.ug.edu.pl>
2017-05-08 18:49:17 +0200

    Change title of app and add Recipe class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+export class Recipe {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Ryourisho';
+  recipe: Recipe = {
+    id: 1,
+    name: 'Warabi Mochi'
+  };
 }
