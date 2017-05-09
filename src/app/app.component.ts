0f4040885d61d0f1d41096e2bca79da15c4dab35
Marcin Kozina <mkozina@sigma.ug.edu.pl>
2017-05-09 18:57:10 +0200

    Add list of selectable recipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,19 @@ export class Recipe {
   name: string;
 }
 
+const RECIPES: Recipe[] = [
+	{ id: 1, name: 'Warabi Mochi' },
+	{ id: 2, name: 'Mushi-pan' },
+	{ id: 3, name: 'Kabocha Squash Pie' },
+	{ id: 4, name: 'Cherry Blossom Milk Pudding' },
+	{ id: 5, name: 'Ogura Toast' },
+	{ id: 6, name: 'Onsen Tamago' },
+	{ id: 7, name: 'Chirashi Sushi' },
+	{ id: 8, name: 'Yakisoba' },
+	{ id: 9, name: 'Miso Katsu' },
+	{ id: 10, name: 'Mapo Tofu' }
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,8 +25,10 @@ export class Recipe {
 })
 export class AppComponent {
   title = 'Ryourisho';
-  recipe: Recipe = {
-    id: 1,
-    name: 'Warabi Mochi'
-  };
+  recipes = RECIPES;
+  selectedRecipe: Recipe;
+
+  onSelect(recipe: Recipe): void {
+    this.selectedRecipe = recipe;
+  }
 }
