2fc2b609a6e82357eac0f068dd3918b14e30bd82
Marcin Kozina <mkozina@sigma.ug.edu.pl>
2017-05-15 10:55:57 +0200

    Add RecipeDetailComponent and bind AppComponent to it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeDetailComponent
   ],
   imports: [
     BrowserModule,
