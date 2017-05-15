9fa4c6041155b1550c96183d96c8481579ff49c5
Marcin Kozina <mkozina@sigma.ug.edu.pl>
2017-05-15 14:35:02 +0200

    Create RecipeService and use it in AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-import { Recipe } from './recipe'
+import { Recipe } from './recipe';
+import { RecipeService } from './recipe.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [RecipeService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Ryourisho';
   recipes: Recipe[];
   selectedRecipe: Recipe;
 
+  constructor(private recipeService: RecipeService) { }
+
+  getRecipes(): void {
+    this.recipeService.getRecipes().then(recipes => this.recipes = recipes);
+  }
+
   onSelect(recipe: Recipe): void {
     this.selectedRecipe = recipe;
   }
+
+  ngOnInit(): void {
+    this.getRecipes();
+  }
+
 }
