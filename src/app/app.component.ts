b853c66af4af832486f5ecda21030d35245085c6
Marcin Kozina <mkozina@sigma.ug.edu.pl>
2017-05-15 12:53:52 +0200

    Move mock recipe data to its own file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,19 +2,6 @@ import { Component } from '@angular/core';
 
 import { Recipe } from './recipe'
 
-const RECIPES: Recipe[] = [
-	{ id: 1, name: 'Warabi Mochi' },
-	{ id: 2, name: 'Mushi-pan' },
-	{ id: 3, name: 'Kabocha Squash Pie' },
-	{ id: 4, name: 'Cherry Blossom Milk Pudding' },
-	{ id: 5, name: 'Ogura Toast' },
-	{ id: 6, name: 'Onsen Tamago' },
-	{ id: 7, name: 'Chirashi Sushi' },
-	{ id: 8, name: 'Yakisoba' },
-	{ id: 9, name: 'Miso Katsu' },
-	{ id: 10, name: 'Mapo Tofu' }
-];
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -22,7 +9,7 @@ const RECIPES: Recipe[] = [
 })
 export class AppComponent {
   title = 'Ryourisho';
-  recipes = RECIPES;
+  recipes: Recipe[];
   selectedRecipe: Recipe;
 
   onSelect(recipe: Recipe): void {
